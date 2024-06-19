f5dc97e3c754666ba5b622070e25d4c6e13b1195
Annie Roldán Rodríguez <149087336+AnnieRoro@users.noreply.github.com>
2024-06-19 12:04:24 +0000

    falta el makefile

--- build/index.js
+++ build/index.js
@@ -1,67 +1,47 @@
-import express from 'express'
-import { locations } from './services/locations'
-
-
-const app = express()
-app.use(express.json())
-
-const PORT = 3000
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const locations_1 = require("./services/locations");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const PORT = 3000;
 app.get('/', (_req, res) => {
-    res.send(locations);
+    res.send(locations_1.locations);
 });
-
-
 app.get('/cities', (_req, res) => {
-    const cities = [...new Set(locations.map(location => location.city))];
+    const cities = [...new Set(locations_1.locations.map(location => location.city))];
     cities.length > 0 ? res.send(cities) : res.status(500).json({ error: 'Error del servidor' });
 });
-
-
 app.get('/districts', (_req, res) => {
-    const districts = [...new Set(locations.map(location => location.district))];
+    const districts = [...new Set(locations_1.locations.map(location => location.district))];
     districts.length > 0 ? res.send(districts) : res.status(500).json({ error: 'Error del servidor' });
 });
-
-
 app.get('/:city/districts', (req, res) => {
     const cityParam = req.params.city.toLowerCase();
-    const districts = locations.filter(location => location.city.toLowerCase() === cityParam);
+    const districts = locations_1.locations.filter(location => location.city.toLowerCase() === cityParam);
     const result = districts.map(location => location.district);
     result.length > 0 ? res.send(result) : res.status(404).json({ error: 'Ciudad no encontrada' });
 });
-
-
 app.get('/:city/units', (req, res) => {
     const cityParam = req.params.city.toLowerCase();
-    const unit = locations.filter(location => location.city.toLowerCase() === cityParam);
+    const unit = locations_1.locations.filter(location => location.city.toLowerCase() === cityParam);
     const result = unit.reduce((sum, location) => sum + location.units, 0);
     result > 0 ? res.json(result) : res.status(404).json({ error: 'Ciudad no encontrada' });
 });
-
-
 app.get('/:district', (req, res) => {
     const disctrictParam = req.params.district.toLowerCase();
-    const unit = locations.filter(location => location.district.toLowerCase() === disctrictParam);
+    const unit = locations_1.locations.filter(location => location.district.toLowerCase() === disctrictParam);
     const result = unit.map(location => location.units);
-    result.length > 0 ? res.send(result) : res.status(404).json({ error: 'Distrito no encontrado' }); 
+    result.length > 0 ? res.send(result) : res.status(404).json({ error: 'Distrito no encontrado' });
 });
-
-
 app.get('/search/:text', (req, res) => {
     const searchText = req.params.text.toLowerCase();
-
-    const cityMatch = locations.find(location => location.city.toLowerCase().includes(searchText));
-    const districtMatch = locations.find(location => location.district.toLowerCase().includes(searchText));
-
-    let bestMatch: {
-        found: boolean;
-        rate?: number;
-        city?: string;
-        name?: string;
-        type?: string;
-    } | null = null;
-
+    const cityMatch = locations_1.locations.find(location => location.city.toLowerCase().includes(searchText));
+    const districtMatch = locations_1.locations.find(location => location.district.toLowerCase().includes(searchText));
+    let bestMatch = null;
     if (cityMatch) {
         bestMatch = {
             found: true,
@@ -70,7 +50,8 @@ app.get('/search/:text', (req, res) => {
             name: cityMatch.city,
             type: 'CITY'
         };
-    } else if (districtMatch) {
+    }
+    else if (districtMatch) {
         bestMatch = {
             found: true,
             rate: searchText.length / districtMatch.district.length,
@@ -81,14 +62,9 @@ app.get('/search/:text', (req, res) => {
     }
     bestMatch ? res.json(bestMatch) : res.status(404).json({ error: 'No se han encontrado ciudades o distritos' });
 });
-
-
 app.use((_req, res) => {
     res.status(404).send({ error: "Endpoint not found" });
 });
-
-
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
 });
-
